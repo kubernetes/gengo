be915022518d97f6ceb80a63757dc13d26ca6ad0
Tim Hockin <thockin@google.com>
2024-01-26 21:57:14 -0800

    v2: Rename DefaultGen -> GolangGenerator
    "Default" is a confusing term here.  Also changed the semantics around
    filename, in prep for subsequent changes to flags.

--- v2/examples/deepcopy-gen/output_tests/generate.go
+++ v2/examples/deepcopy-gen/output_tests/generate.go
@@ -1,2 +1,2 @@
-//go:generate go run k8s.io/gengo/v2/examples/deepcopy-gen -i k8s.io/gengo/v2/examples/deepcopy-gen/output_tests/... -O zz_generated --go-header-file=../../../boilerplate/boilerplate.go.txt
+//go:generate go run k8s.io/gengo/v2/examples/deepcopy-gen -i k8s.io/gengo/v2/examples/deepcopy-gen/output_tests/... -O zz_generated.go --go-header-file=../../../boilerplate/boilerplate.go.txt
 package output_tests

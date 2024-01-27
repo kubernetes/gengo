3d41cf4777161e0f9336ac0c2c47e610befbaf97
Tim Hockin <thockin@google.com>
2024-01-27 13:15:04 -0800

    v2: Make the OutputBase flag each tool's problem
    In this repo that is just pointuh.  Also rename to --output-dir.

--- v2/hack/verify-examples.sh
+++ v2/hack/verify-examples.sh
@@ -63,7 +63,7 @@ fi
 rm -rf ./examples/pointuh/testdata/results
 go run ./examples/pointuh/ \
     -i ./examples/pointuh/testdata/simple \
-    -o ./examples/pointuh/testdata/results \
+    --output-dir ./examples/pointuh/testdata/results \
     --output-package k8s.io/gengo/examples/pointuh/testdata/results
 if ! git diff --quiet HEAD; then
     echo "FAIL: output files changed"
